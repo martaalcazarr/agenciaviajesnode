caeaf1e0e05cf2a5b6094126e89f908a7ab2ff82
Marta Alcázar Redondo <108906104+martaalcazarr@users.noreply.github.com>
2023-02-04 22:50:02 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,8 +9,9 @@ console.log(process.env.DB_HOST)
 //variables de entorno para no mostrar la información privada. las variables de entorno
 //se ven de una forma al desarrollar y de otra al colgar la pagina online
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
-    host: process.env.DB_HOST,
-    port: '3306',
+    //host: process.env.DB_HOST,
+    host: '0.0.0.0',
+    port: '7530',
     dialect: 'mysql',
     define:{
         timestamps: false
